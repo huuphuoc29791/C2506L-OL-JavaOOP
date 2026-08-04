a501e0e7128dc75c4b53d367e3de6e1720f21f34
Ricky Duong <huuphuoc29791@gmail.com>
2026-08-04 20:42:26 +0700

    OOP Buoi 3 - 04/08 - Time.java

--- src/App.java
+++ src/App.java
@@ -1,8 +1,15 @@
+import java.util.Scanner;
 
 public class App {
     public static void main(String[] args) {
-        Triangle t = new Triangle();
-        t.input();
-        t.output();
+        Time A = new Time();
+        Time B = new Time();
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Input time A: ");
+        A.input(scanner);
+        System.out.print("Input time B: ");
+        B.input(scanner);
+        System.out.print("Time difference: ");
+        A.difference(B).output();
     }
 }
