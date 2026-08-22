701c43f904afc3f02b8b60cb60fd6f82307dafd5
Ricky Duong <huuphuoc29791@gmail.com>
2026-08-22 21:07:26 +0700

    OOP Buoi 11 - 22/08 - Student Management with File - Part 1

--- src/App.java
+++ src/App.java
@@ -1,17 +1,41 @@
 
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.util.ArrayList;
+
 public class App {
     public static void main(String[] args) {
-        BankAccount acc = new BankAccount("VCB", 1000);
-        acc.displayBalance();
-
-        acc.deposit(325);
-        acc.displayBalance();
-
-        try {
-            acc.withdraw(5000);
-            acc.displayBalance();
-        } catch (InsufficientBalanceException e) {
-            System.out.println(e.getMessage());
+        ArrayList<Student> students = loadStudents("students.csv");
+        for (Student s : students) {
+            System.out.println(s);
         }
+    }
+
+    public static void saveStudents(ArrayList<Student> students, String fileName) {
+        try (BufferedWriter bw = new BufferedWriter(new FileWriter(fileName))) {
+            for (Student s : students) {
+                bw.write(String.format("%s,%s,%f", s.getId(), s.getName(), s.getGPA()));
+                bw.newLine();
+            }
+        } catch (IOException e) {
+            System.out.println("Cannot save students to file: " + e.getMessage());
+        }
+    }
+
+    public static ArrayList<Student> loadStudents(String fileName) {
+        ArrayList<Student> students = new ArrayList<Student>();
+        try (BufferedReader br = new BufferedReader(new FileReader(fileName))) {
+            String line;
+            while ((line = br.readLine()) != null) {
+                String[] data = line.split(",");
+                students.add(new Student(data[0], data[1], Double.parseDouble(data[2])));
+            }
+        } catch (IOException e) {
+            System.out.println("Cannot load students: " + e.getMessage());
+        }
+        return students;
     }
 }
---
+++ src/Student.java
@@ -0,0 +1,28 @@
+public class Student {
+    private String id;
+    private String name;
+    private double gpa;
+
+    public Student(String id, String name, double gpa) {
+        this.id = id;
+        this.name = name;
+        this.gpa = gpa;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public double getGPA() {
+        return gpa;
+    }
+
+    @Override
+    public String toString() {
+        return String.format("ID: %s - Name: %s - GPA: %.1f", id, name, gpa);
+    }
+}
